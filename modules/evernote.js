a1686200e1913ba2189ca0c34d7216aa3569c99c
Piotr Plewa <pplewa@atlassian.com>
2016-04-19 14:32:11 +1000

    reporter update

--- modules/evernote.js
+++ modules/evernote.js
@@ -97,13 +97,11 @@ exports.getMemories = function() {
 		ascending: false
 	});
 
-	var foodWords = interpolate('notebook:food created:{dateFormat1} -created:{dateFormat2}', {
-		dateFormat1: moment().tz(config.TIMEZONE).subtract(config.DAYS_AGO, 'day').format('YYYYMMDD'),
-		dateFormat2: moment().tz(config.TIMEZONE).subtract(config.DAYS_AGO - 1, 'day').format('YYYYMMDD')
-	});
-	console.log(foodWords);
 	var foodFilter = new Evernote.NoteFilter({
-		words: foodWords,
+		words: interpolate('notebook:food created:{dateFormat1} -created:{dateFormat2}', {
+			dateFormat1: moment().tz(config.TIMEZONE).subtract(config.DAYS_AGO, 'day').format('YYYYMMDD'),
+			dateFormat2: moment().tz(config.TIMEZONE).subtract(config.DAYS_AGO - 1, 'day').format('YYYYMMDD')
+		}),
 		order: Evernote.NoteSortOrder.CREATED,
 		ascending: false
 	});
@@ -129,6 +127,7 @@ exports.getMemories = function() {
 				});
 			});
 			noteStore.findNotesMetadata(foodFilter, 0, 20, noteSpec, function(error, data){
+				console.log(JSON.stringify(data));
 				data.notes.forEach(function(note){
 					memories.push({
 						food: true,
